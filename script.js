74afcb15c000ff04ab6d0f81661f7745000ab0d6
sahilsharma0012 <148423581+sahilsharma0012@users.noreply.github.com>
2023-10-19 03:20:12 -0700

    index.html

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function loco() {
+function init() {
   gsap.registerPlugin(ScrollTrigger);
 
   const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
+    el: document.querySelector(".main"),
     smooth: true,
   });
-
   locoScroll.on("scroll", ScrollTrigger.update);
 
-  ScrollTrigger.scrollerProxy("#main", {
+  ScrollTrigger.scrollerProxy(".main", {
     scrollTop(value) {
       return arguments.length
         ? locoScroll.scrollTo(value, 0, 0)
@@ -22,7 +21,7 @@ function loco() {
         height: window.innerHeight,
       };
     },
-    pinType: document.querySelector("#main").style.transform
+    pinType: document.querySelector(".main").style.transform
       ? "transform"
       : "fixed",
   });
@@ -32,167 +31,85 @@ function loco() {
   ScrollTrigger.refresh();
 }
 
-// loco();
+init();
 
-function cursor() {
-  var main = document.querySelector("#main");
+var crsr = document.querySelector(".cursor");
+var main = document.querySelector(".main");
+main.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+});
 
-  main.addEventListener("mousemove", function (dets) {
-    document.querySelector(".cursor").style.left = `${dets.clientX}px`;
-    document.querySelector(".cursor").style.top = `${dets.clientY}px`;
-  });
-}
+// Jo humne timeline mein likha hai, vo saare properties ab hum chahey kise bhi element ko de skate hai..
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: " .page1 h1",
+    scroller: ".main",
+    // markers: true,
+    start: "top 30%",
+    end: "top 0%",
+    scrub: 3,
+  },
+});
 
-cursor();
+tl.to(
+  ".page1 h1",
+  {
+    x: -100,
+    duration: 1,
+    linear: "power4.in",
+  },
+  "sahil" // Koi same name dene se dono ko, vo dono ab ek sath chelege
+);
 
-function revealToSpan() {
-  document.querySelectorAll(".reveal").forEach(function (elem) {
-    // creating two span
-    var parent = document.createElement("span");
-    var child = document.createElement("span");
+// Jo properties humne page1 k h1 ko de thi, ab vo saare properties ab page1 k h2 ko bhi mil jayege, scroll trigger wale.
+tl.to(
+  ".page1 h2",
 
-    //   adding classes
-    parent.classList.add("parent");
-    child.classList.add("child");
-
-    child.innerHTML = elem.innerHTML;
-    parent.appendChild(child);
-
-    elem.innerHTML = "";
-    elem.appendChild(parent);
-  });
-}
-
-revealToSpan();
-
-function loaderAnimation() {
-  var tl = gsap.timeline();
-
-  tl.from(".child span", {
-    opacity: 0.1,
+  {
     x: 100,
-    stagger: 0.2,
-    duration: 1.4,
-    ease: Power3.easeInOut,
-  })
-    .to(".parent .child", {
-      y: "-100%",
-      duration: 1,
-      ease: Circ.easeInOut,
-    })
-    .to("#loader", {
-      height: 0,
-      duration: 1,
-      delay: -0.5,
-      ease: Circ.easeInOut,
-    })
+    duration: 1,
+    linear: "power4.in",
+  },
+  "sahil" // Koi same name dene se dono ko, vo dono ab ek sath chelege
+);
 
-    .to("#green", {
-      height: "90vh",
-      top: "0vh",
-      duration: 0.8,
-      delay: -0.9,
-      ease: Circ.easeInOut,
-    })
-    .to("#green", {
-      height: "0vh",
-      duration: 0.5,
-      delay: -0.5,
-      ease: Circ.easeInOut,
-    });
+tl.to(
+  ".page1 video",
+  {
+    width: "90%",
+  },
+  "sahil"
+);
 
-  gsap.set("#home", {
-    opacity: 0,
-    y: 100,
-  });
+//page2
 
-  tl.to("#home", {
-    y: 0,
-    ease: Power2.easeInOut,
-    duration: 0.5,
-    opacity: 1,
-  });
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: " .page1 h1",
+    scroller: ".main",
+    // markers: true,
+    start: "top -100%",
+    end: "top -400",
+    scrub: 3,
+  },
+});
 
-  tl.from(".row h1", {
-    scale: 0.5,
-    opacity: 0,
-    x: 200,
-    stagger: 0.1,
-  });
+tl2.to(".main", {
+  backgroundColor: "#fff",
+});
 
-  tl.from(".text h5", {
-    opacity: 0,
-    scale: 0.5,
-  });
-}
+var tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: " .page1 h1",
+    scroller: ".main",
+    // markers: true,
+    start: "top -200%",
+    end: "top -210",
+    scrub: 3,
+  },
+});
 
-loaderAnimation();
-
-function clock() {
-  gsap.to("#circle", {
-    rotate: 0,
-    ease: "cubic-bezier(0.19, 1, 0.22, 1)",
-    duration: 2,
-  });
-
-  var active = 3;
-
-  var mncircles = document.querySelectorAll(".mncircel");
-  var sec = document.querySelectorAll(".sec");
-
-  gsap.to(mncircles[active - 1], {
-    opacity: 1,
-  });
-
-  gsap.to(sec[active - 1], {
-    opacity: 1,
-  });
-
-  mncircles.forEach(function (val, index) {
-    val.addEventListener("click", function () {
-      gsap.to("#circle", {
-        rotate: 3 - (index + 1) * 10,
-        ease: Expo.easeInOut,
-        duration: 1,
-      });
-      greyout();
-
-      gsap.to(this, {
-        opacity: 1,
-      });
-
-      gsap.to(sec[index], {
-        opacity: 1,
-      });
-    });
-  });
-
-  function greyout() {
-    gsap.to(mncircles, {
-      opacity: 0.3,
-    });
-
-    gsap.to(sec, {
-      opacity: 0.4,
-    });
-  }
-}
-
-// clock();
-
-function hidden() {
-  var hiddenText = document.querySelector(".hidden-text");
-  var btn = document.querySelector(".btn-box");
-  var close = document.querySelector("#close");
-  btn.addEventListener("click", function () {
-    hiddenText.style.display = "block";
-    btn.style.display = "none";
-  });
-
-  close.addEventListener("click", function () {
-    hiddenText.style.display = "none";
-    btn.style.display = "block";
-  });
-}
-
-hidden();
+tl3.to(".main", {
+  backgroundColor: "#0f0d0d",
+});
