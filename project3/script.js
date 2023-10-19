5e24a628db69a335360a922eb9c7d7508026cdce
sahilsharma0012 <148423581+sahilsharma0012@users.noreply.github.com>
2023-10-19 04:06:31 -0700

    index.html

---
+++ project3/script.js
@@ -0,0 +1,26 @@
+gsap.from("#image1", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: 60,
+});
+
+gsap.from("#image2", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  x: 60,
+});
+
+gsap.from("#image3", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: -60,
+});
+
+gsap.from("#main h1", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+});
